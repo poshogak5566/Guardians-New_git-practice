49e11b156c2a94b35a0e255d925356bb50317495
qudsia <q.yousefzai@gmail.com>
2023-08-27 11:49:31 -0700

    Adding another class to pjt

---
+++ src/Api_prct/NewClass.java
@@ -0,0 +1,12 @@
+package Api_prct;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I love coding");
+
+	}
+
+}
